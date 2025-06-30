fbe3f0a5257ca450d631fed9c0c38d65ca9354a0
lestorialqs <henry.quispe.s@utec.edu.pe>
2025-06-29 19:25:09 -0500

    Add files via upload

--- comentario.py
+++ comentario.py
@@ -20,6 +20,17 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+    # Guardar el comentario en S3
+    s3 = boto3.client('s3')
+    bucket_name = os.environ["BUCKET_NAME"]
+    file_name = f"{tenant_id}/{uuidv1}.json"
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=str(comentario),
+        ContentType='application/json'
+    )
+    
     # Salida (json)
     print(comentario)
     return {
